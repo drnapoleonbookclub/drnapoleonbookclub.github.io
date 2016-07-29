e3ad26ac8141260aeb08a2350128a0c07295ff68
DrNapoleonBookClub <drnapoleonbookclub@yandex.com>
2016-07-29 23:59:44 +0000

    null

--- _posts/2016-07-30-Index.ml
+++ _posts/2016-07-30-Index.ml
@@ -1,13 +1,14 @@
 ---
 layout: post
-title: Example content
+title: How to participate
 ---
 
 
 <div class="message">
-  Howdy! This is an example blog post that shows several types of HTML content supported in this theme.
+ Unofficial book club for discussion of Dr Anthony Napoleon's books, white papers, and commentary.
 </div>
 
+To view all discussions, please visit the index.
 Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. *Aenean eu leo quam.* Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
 
 > Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.
