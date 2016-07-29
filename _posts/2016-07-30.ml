d1165b88f680da90ec98cb4da90c69a105990146
DrNapoleonBookClub <drnapoleonbookclub@yandex.com>
2016-07-29 23:47:55 +0000

    index

---
+++ _posts/2016-07-30.ml
@@ -0,0 +1,24 @@
+---
+layout: post
+comments: False
+title: About
+
+<p class="message">
+  Unofficial book club for discussion of Dr Anthony Napoleon's books, white papers, and commentary.
+</p>
+
+To view all discussions, please visit the site <a href="/archive">index</a>.
+
+## Books
+
+* Awakening Beauty: An Illustrated Look at Mankind's Love and Hatred of Beauty
+* A Look Inside the Playbook - How Marxists Plan to Destroy America
+* The Progressive Virus: Why You Cannot Permit It to Go Forward
+* Shadow Men: An Encyclopedia of Mind Control
+* Federal Grand Jury Transcript
+
+## White Papers
+* Epistimology
+* Trans Jenner
+
+For more information and to purchase these titles visit [Dr Napoleon Books](http://www.drnapoleonbooks.com)
