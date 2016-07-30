0b4594a8022575f126d18731d330ec5a58bc5339
DrNapoleonBookClub <drnapoleonbookclub@yandex.com>
2016-07-30 00:42:08 +0000

    null

---
+++ _posts/2016-07-30-General.ml
@@ -0,0 +1,7 @@
+---
+layout: post
+title: General
+comments: True
+---
+
+General discussion: suggest new topics in the Disqus comments below or visit the <a href="/archive">index of existing topics</a>.
