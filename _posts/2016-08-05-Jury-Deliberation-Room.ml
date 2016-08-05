720c59ed9b0ef522add66808796e5f8528f8aa00
DrNapoleonBookClub <drnapoleonbookclub@yandex.com>
2016-08-05 19:34:00 +0000

    null

--- _posts/2016-08-05-Jury-Deliberation-Room.ml
+++ _posts/2016-08-05-Jury-Deliberation-Room.ml
@@ -8,4 +8,4 @@ category: FederalGrandJuryTranscript
 <p>"Well ladies and gentlemen, that time has come. We have a beautiful deliberation room set up for you with comfortable seating and a mini-refrigerator filled with soft drinks and water for you. If you need anything you may buzz the bailiff who will be stationed outside of the deliberation room during the process."</p>
 <br />
 <p>"Please now retire to the deliberation room and begin the process."</p>
-<p><q><em>Federal Grand Jury Transcript by Dr. Anthony Napoleon</p></q></em>
+<p><q><em><strong>Federal Grand Jury Transcript</strong> by Dr. Anthony Napoleon</p></q></em>
