33a9c59f57bd2b6ea7bc6c19aaf31b1f9f54039c
DrNapoleonBookClub <drnapoleonbookclub@yandex.com>
2016-08-05 19:25:36 +0000

    null

--- _posts/2016-08-05-Jury-Deliberation-Room.ml
+++ _posts/2016-08-05-Jury-Deliberation-Room.ml
@@ -4,4 +4,7 @@ title: Jury Deliberation Room
 comments: True
 category: FederalGrandJuryTranscript
 ---
-Test
+
+Well ladies and gentlemen, that time has come. We have a beautiful deliberation room set up for you with comfortable seating and a mini-refrigerator filled with soft drinks and water for you. If you need anything you may buzz the bailiff who will be stationed outside of the deliberation room during the process.
+
+Please now retire to the deliberation room and begin the process.
