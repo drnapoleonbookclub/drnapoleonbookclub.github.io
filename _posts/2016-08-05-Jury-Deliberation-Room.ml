3e68d7df9c3c173d7ff1794dce566a536fcd398c
DrNapoleonBookClub <drnapoleonbookclub@yandex.com>
2016-08-05 19:32:50 +0000

    null

--- _posts/2016-08-05-Jury-Deliberation-Room.ml
+++ _posts/2016-08-05-Jury-Deliberation-Room.ml
@@ -5,6 +5,7 @@ comments: True
 category: FederalGrandJuryTranscript
 ---
 
-<p>Well ladies and gentlemen, that time has come. We have a beautiful deliberation room set up for you with comfortable seating and a mini-refrigerator filled with soft drinks and water for you. If you need anything you may buzz the bailiff who will be stationed outside of the deliberation room during the process.</p>
+<p>"Well ladies and gentlemen, that time has come. We have a beautiful deliberation room set up for you with comfortable seating and a mini-refrigerator filled with soft drinks and water for you. If you need anything you may buzz the bailiff who will be stationed outside of the deliberation room during the process."</p>
 <br />
-<p>Please now retire to the deliberation room and begin the process.</p>
+<p>"Please now retire to the deliberation room and begin the process."</p>
+<quote><p>Federal Grand Jury Transcript by Dr. Anthony Napoleon</p></quote>
